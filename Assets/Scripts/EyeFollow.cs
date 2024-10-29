bcf2df54bee02305c2ec28911657cf4cb482741b
nuno <ruiduidahh@Gmail.com>
2024-10-29 15:53:52 +0100

    Crowd + Lights Latest

---
+++ Assets/Scripts/EyeFollow.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeFollow : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        eyeFollow();
+    }
+
+    void eyeFollow(){
+        Vector3 playerpos = player.transform.position;
+
+        Vector2 direction = new Vector2(
+            playerpos.x - transform.position.x,
+            playerpos.y - transform.position.y
+        );
+        transform.up = direction;
+        
+    }
+}
